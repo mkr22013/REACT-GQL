3d1c277519a249089c601a6554f5cb61d9d942e6
mkr22013 <mkr22013@outlook.com>
2025-02-07 01:30:18 -0800

    fixed contact us page

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -4,10 +4,25 @@ import Modal from "../Popup/Popup";
 function ContactUs() {
   const [msg, setMsg] = useState();
   const [showPopup, setShowPopup] = useState(false);
-  function SendEmail() {
-    setMsg("Message has been sent successfully");
-    setShowPopup(true);
+
+  const SendEmail = async () => {
+    try {
+      setShowPopup(false);
+      await sleep(1000); //this method wait for 10 sec.
+      setMsg("Message has been sent successfully");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setShowPopup(true);
+    }
+  };
+
+  function sleep(time) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, time || 1000);
+    });
   }
+
   return (
     <div className="max-w-7xl mx-auto p-4 md:p-8">
       <div className="grid md:grid-cols-2 gap-8 items-start">
